294bb6bd5e16d0b3f5002be8f5272da1580ae62d
DominionWind <alexczr@ukr.net>
2018-04-05 00:07:07 +0300

    Tugovasto

--- src/lesson11/HomeWork/BookingComAPI.java
+++ src/lesson11/HomeWork/BookingComAPI.java
@@ -1,9 +1,22 @@
 package lesson11.HomeWork;
 
-public class BookingComAPI implements API{
+public class BookingComAPI implements API {
+
+    Room[] rooms;
+
+    public BookingComAPI(Room[] rooms) {
+        this.rooms = rooms;
+    }
 
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
+        int count = 0;
+        for (Room room : rooms) {
+                if (room != null && (price + 100) >= room.getPrice() && (price - 100) <= room.getPrice() && persons == room.getPersons() && city == room.getCityName() && hotel == room.getHotelName()) {
+                    count++;
+            }
+        }
+
         return new Room[0];
     }
 
