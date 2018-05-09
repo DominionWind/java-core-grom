1341c305e3d51c90f2bb52ac7db056c30d9bf8f4
DominionWind <alexczr@ukr.net>
2018-05-09 22:48:11 +0300

    lesson 16 HW

--- src/lesson16/HomeWork/Solution.java
+++ src/lesson16/HomeWork/Solution.java
@@ -1,12 +1,26 @@
 package lesson16.HomeWork;
 
+import java.util.Arrays;
+
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(minWord("Some text is tuping right here"));
-        System.out.println(maxWord("Some text is tuping right here"));
-        System.out.println(minWord("           "));
-        System.out.println(maxWord("           "));
 
+        //validate("http://someMail@grom.com");
+
+//        System.out.println(minWord("Some text is tuping right here"));
+//        System.out.println(maxWord("Some text is tuping right here"));
+//        System.out.println(minWord("           "));
+//        System.out.println(maxWord("           "));
+
+//        System.out.println(validate("http://someMail@grom.com"));
+//        System.out.println(validate("http://some123Mail@grom.com"));
+
+//        System.out.println(dogTest("http://someMail@grom.com"));
+//        System.out.println(dogTest("http://someMail@@grom.com"));
+//        System.out.println(worldsWalid("someMail@grom"));
+//        System.out.println(worldsWalid("some//123Mail@grom"));
+//        System.out.println(wordCheck("someMail"));
+//        System.out.println(wordCheck("some//123Mail"));
     }
 
     public int countWords(String input) {
@@ -32,16 +46,16 @@ public class Solution {
         String maxWord = null;
         int max = worlds[0].length();
 
-        if (input != null && !input.isEmpty())
-        for (int i = 0; i < input.length(); i++) {
-            if (worlds[i].length() > max) {
-                max = worlds[i].length();
-                for (int j = 0; j < input.length(); j++) {
-                    if (worlds[j].length() == max)
-                        return maxWord = worlds[j];
+        if (input != null && !input.isEmpty() && input.length() > 0)
+            for (int i = 0; i < input.length(); i++) {
+                if (worlds[i].length() > max && worlds[i].length() > 0 && wordCheck(worlds[i])) {
+                    max = worlds[i].length();
+                    for (int j = 0; j < input.length(); j++) {
+                        if (worlds[j].length() == max && worlds[j].length() > 0 && wordCheck(worlds[j]))
+                            return maxWord = worlds[j];
+                    }
                 }
             }
-        }
         return maxWord;
     }
 
@@ -51,15 +65,96 @@ public class Solution {
         int min = worlds[0].length();
 
         if (input != null && !input.isEmpty())
-        for (int i = 0; i < input.length(); i++) {
-            if (worlds[i].length() < min) {
-                min = worlds[i].length();
-                for (int j = 0; j < input.length(); j++) {
-                    if (worlds[j].length() == min)
-                        return minWord = worlds[j];
+            for (int i = 0; i < input.length(); i++) {
+                if (worlds[i].length() < min && worlds[i].length() > 0 && wordCheck(worlds[i])) {
+                    min = worlds[i].length();
+                    for (int j = 0; j < input.length(); j++) {
+                        if (worlds[j].length() == min && worlds[j].length() > 0 && wordCheck(worlds[j]))
+                            return minWord = worlds[j];
+                    }
+                }
+            }
+        return minWord;
+    }
+
+    public static String mostCountedWorld(String input) {
+        String[] worlds = input.split(" ");
+        int count = 0;
+        int maxCount = 0;
+        String reiterationWord = null;
+
+
+        for (String word : worlds) {
+            for (String w : worlds) {
+                if (w.equals(word) && word.length() > 0 && wordCheck(word))
+                    count++;
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                reiterationWord = word;
+            }
+        }
+        return reiterationWord;
+    }
+
+    public static boolean validate(String adress) { //http://www.someMail@grom.com
+        if (adress.endsWith(".com") || adress.endsWith(".net") || adress.endsWith(".org")) {
+            String text = adress.substring(0, adress.length() - 4);
+            if (adress.startsWith("http://")) {
+                String devText = text.substring(7);
+                dogTest(devText);
+                worldsWalid(devText);
+                System.out.println(dogTest(devText) + "1");
+                System.out.println(worldsWalid(devText) + "2");
+            } else if (adress.startsWith("https://")) {
+                String devText = text.substring(8);
+                dogTest(devText);
+                worldsWalid(devText);
+                System.out.println(dogTest(devText) + "3");
+                System.out.println(worldsWalid(devText) + "4");
+            }
+        }
+        return false;
+    }
+
+    private static boolean dogTest(String input) {
+        char[] chars = input.toCharArray();
+        int count = 0;
+        for (char c : chars) {
+            if (c == '@') {
+                count++;
+                if (count > 1) {
+                    return false;
                 }
             }
         }
-        return minWord;
+        return true;
+    }
+
+    private static boolean znakTest(String input) {
+        String[] test = input.split("://");
+        if (test.length > 1) ;
+        return false;
+    }
+
+    private static boolean worldsWalid(String input) {
+        String[] worlds = input.split("@");
+        for (String world : worlds) {
+            if (wordCheck(world) == false)
+                return false;
+        }
+        return true;
+    }
+
+
+    private static boolean wordCheck(String input) {
+        char[] letters = input.toCharArray();
+        for (char letter : letters) {
+            if (!Character.isLetter(letter)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
+
