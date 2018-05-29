34617f2e8b15af8dbca5d897e0e6fbd07696dd72
DominionWind <alexczr@ukr.net>
2018-05-29 22:53:29 +0300

    lesson 19 HW

---
+++ src/lesson19/HomeWork/Controller.java
@@ -0,0 +1,20 @@
+package lesson19.HomeWork;
+
+public class Controller {
+
+    public void put(Storage storage, File file){
+
+    }
+
+    public void delete(Storage storage, File file){
+
+    }
+
+    public void transferAll(Storage storageFrom, Storage storageTo){
+
+    }
+
+    public void transferFile(Storage storageFrom, Storage storageTo, long id){
+
+    }
+}
