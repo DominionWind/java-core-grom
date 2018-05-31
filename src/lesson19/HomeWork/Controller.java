fe8c31288f2103643c7fb249e596b6619e9bf06b
DominionWind <alexczr@ukr.net>
2018-05-31 23:05:12 +0300

    lesson 19 HW

--- src/lesson19/HomeWork/Controller.java
+++ src/lesson19/HomeWork/Controller.java
@@ -1,5 +1,7 @@
 package lesson19.HomeWork;
 
+import lesson9.HomeWork.User;
+
 public class Controller {
 
     public void put(Storage storage, File file) {
@@ -18,16 +20,67 @@ public class Controller {
 
     }
 
+//    public User save(User user) {
+//        if (findById(user.getId()) == null)
+//            for (int i = 0; i < users.length; i++)
+//                if (users[i] == null) {
+//                    users[i] = user;
+//                    return user;
+//                }
+//        return null;
+//    }
+//
+//    public User update(User user) {
+//        if (user != null)
+//            return null;
+//        for (int i = 0; i < users.length; i++) {
+//            if (users[i].equals(findById(user.getId()))){
+//                users[i] = user;
+//                return users[i];
+//            }
+//            return null;
+//        }
+//        return null;
+//    }
+//
+//
+//    public void delete(long id) {
+//        if (findById(id) != null)
+//            for (int i = 0; i < users.length; i++)
+//                if (users[i].equals(findById(id)))
+//                    users[i] = null;
+//    }
+
+
     private boolean checkFormat(Storage storage, File file) throws Exception {
         String mesage = "Storage ID# " + storage.getId() + "doesn`t support " + file.getName() + file.getFormat() + " format. Everybody Dies=(";
 
-        for (int i=0; i<storage.getFormatsSupported().length; i++){
+        for (int i = 0; i < storage.getFormatsSupported().length; i++) {
             if (file.getFormat().equals(storage.getFormatsSupported())) {
                 return true;
             }
             throw new Exception(mesage);
         }
         return false;
+    }
 
+    private boolean checkId(Storage storage, File file) throws Exception {
+        String mesage = "Fatal Error. File whith ID" + file.getId() + " already in Storage ID#" + storage.getId();
+        for (int i = 0; i < storage.getFiles().length; i++) {
+            for (File f : storage.getFiles()) {
+                if (file.getId() == f.getId()) {
+                    return false;
+//                    throw new Exception(mesage);
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean checkSize(Storage storage, File file) {
+        if (storage.getStorageSize() - storage.getFiles().length > 0) {
+            return true;
+        }
+        return false;
     }
 }
