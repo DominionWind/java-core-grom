6246ca1a212eaecfb8d157c9e1cd0cf23bf169a8
DominionWind <alexczr@ukr.net>
2018-06-04 00:13:21 +0300

    lesson 19 HW

--- src/lesson19/HomeWork/Demo.java
+++ src/lesson19/HomeWork/Demo.java
@@ -1,7 +1,24 @@
 package lesson19.HomeWork;
 
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        Controller controller = new Controller();
+
+        File file1 = new File(123, "LongLong", "txt", 32167);
+        File file2 = new File(234, "somePDF", "pdf", 258);
+        File file3 = new File(234, "somePNG", "png", 888);
+        File file4 = new File(456, "someEXE", "exe", 456);
+        File file5 = new File(567, "someTXT", "txt", 231);
+
+        File file6 = new File(777, "someTXT", "txt", 231);
+
+        File[] array1 = {file1, file2, file3, file4, file5, null, null};
+        File[] array2 = {null, null, null, null, null};
+
+        Storage storage1 = new Storage(1111, array1, new String[]{"txt", "pdf", "png", "exe"}, "Ukraine", 999999);
+
+        System.out.println(controller.put(storage1,file6));
 
     }
 }
