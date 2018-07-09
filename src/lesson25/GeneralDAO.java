903260457588a66f2bb2f175abb1d5491194a43f
DominionWind <alexczr@ukr.net>
2018-07-09 15:31:38 +0300

    Lesson 25 HW

--- src/lesson25/GeneralDAO.java
+++ src/lesson25/GeneralDAO.java
@@ -1,19 +1,66 @@
 package lesson25;
 
-public class GeneralDAO {
+public class GeneralDAO<T> {
 
-    private void print(Order order){
+    private T[] DB = (T[]) new Object[10];
+
+    public T save(T t) throws Exception{
+        
+        if (t == null) {
+            return null;
+        }
+
+        int freeSlotsCount = 0;
+        for (T el:DB){
+            if (el==null);
+            freeSlotsCount++;
+        }
+
+        if (freeSlotsCount<=0){
+            throw new Exception("Not enought free slots in DB. Can`t save "+ t);
+        }
+
+        for (int i = 0; i < DB.length; i++) {
+            if (DB[i] == null) {
+                DB[i] = t;
+                return DB[i];
+            }
+        }
+        return t;
+    }
+
+    public T getAll() {
+        int count = 0;
+        for (T el : DB) {
+            if (el != null)
+                count++;
+        }
+
+        T[] DataBase = (T[]) new Object[count];
+
+        int i = 0;
+        for (T el : DB) {
+            if (el != null) {
+                DataBase[i] = el;
+                i++;
+            }
+        }
+        return (T) DataBase;
+    }
+
+
+    private void print(Order order) {
         System.out.println("order is:" + order.toString());
     }
 
-    public <T extends IdEntity> void validate(T t)throws Exception{
-        if (t.getId()<=0)
+    public <T extends IdEntity> void validate(T t) throws Exception {
+        if (t.getId() <= 0)
             throw new Exception("id can`t be negative");
     }
 
-    public <K> void validate(K k){
+    public <K> void validate(K k) {
 
-        if (k.equals(100)){
+        if (k.equals(100)) {
             System.out.println("true");
         } else {
             System.out.println("false");
