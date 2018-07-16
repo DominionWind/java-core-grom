da1d017efa062a8da5d3656a2818543321a004a0
DominionWind <alexczr@ukr.net>
2018-07-16 22:51:40 +0300

    lesson 28 HW

--- src/lesson28/FullComparator.java
+++ src/lesson28/FullComparator.java
@@ -2,35 +2,37 @@ package lesson28;
 
 import java.util.Comparator;
 
-public class FullComparator implements Comparator<Capability> {
+public class FullComparator<T> implements Comparator<Capability> {
     @Override
     public int compare(Capability o1, Capability o2) {
 
-
-        if (o1.getChannelName() == null && o2.getChannelName() == null) {
-            return 0;
-        } else if (o1.getChannelName() == null) {
-            return 1;
-        } else if (o2.getChannelName() == null) {
-            return -1;
-        } else if (!o1.getChannelName().equals(o2)) {
-            return o1.getChannelName().compareTo(o2.getChannelName());
-        } else if (o1.getFingerprint() == null && o2.getFingerprint() == null) {
-            return 0;
-        } else if (o1.getFingerprint() == null) {
-            return 1;
-        } else if (o2.getFingerprint() == null) {
-            return -1;
-        } else if (!o1.getFingerprint().equals(o2))
-            return o1.getFingerprint().compareTo(o2.getFingerprint());
-        else if (o1.getDateCreated() == null && o2.getDateCreated() == null) {
-            return 0;
-        } else if (o1.getDateCreated() == null) {
-            return 1;
-        } else if (o2.getDateCreated() == null) {
-            return -1;
-        } else {
-            return (int) (o1.getDateCreated().getTime() - o2.getDateCreated().getTime());
+        if (o1.getChannelName() == null || o2.getChannelName() == null) {
+            check(o1.getChannelName(), o2.getChannelName())
         }
+
+        if (!o1.getChannelName().equals(o2)) {
+            return o1.getChannelName().compareTo(o2.getChannelName());
+        }
+
+        if (o1.getFingerprint() == null || o2.getFingerprint() == null) {
+            check(o1.getFingerprint(), o2.getFingerprint())
+        }
+
+        if (!o1.getFingerprint().equals(o2)){
+            return o1.getFingerprint().compareTo(o2.getFingerprint());
+        }
+
+        return (int) (o1.getDateCreated().getTime() - o2.getDateCreated().getTime());
+    }
+}
+
+    private int check(T a, T b) {
+
+        if (a == null && b == null) {
+            return 0;
+        } else if (a == null) {
+            return 1;
+        } else
+            return -1;
     }
 }
