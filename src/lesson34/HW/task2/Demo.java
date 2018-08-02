12c2ad56ea18bf9f660f797e879d5822b48945e0
DominionWind <alexczr@ukr.net>
2018-08-03 00:26:50 +0300

    lesson 34 HW task 2

---
+++ src/lesson34/HW/task2/Demo.java
@@ -0,0 +1,9 @@
+package lesson34.HW.task2;
+
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+
+        solution.transferSentences("E:\\Games\\test1.txt", "E:\\Games\\test2.txt", "test");
+    }
+}
